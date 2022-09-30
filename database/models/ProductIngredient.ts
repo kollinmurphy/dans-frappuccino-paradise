bd8d8f02207bd9073c4ec02b1ad698d3a6327236
belnajoh <john@belnap.org>
2022-09-30 12:17:36 -0600

    adding product ingredient to database

---
+++ database/models/ProductIngredient.ts
@@ -0,0 +1,43 @@
+import { DataTypes, InferAttributes, InferCreationAttributes, Model, Sequelize } from "sequelize";
+
+
+const ProductIngredient = (sequelize: Sequelize) => {
+    class ProductIngredient extends Model<InferAttributes<ProductIngredient>, InferCreationAttributes<ProductIngredient>> {
+        declare name: string;
+        declare price: number;
+        declare quantityOnHand: number;
+        declare createdAt: Date;
+        declare updatedAt: Date;
+
+
+        static associate(models: any) {
+
+
+        }
+    }
+
+    ProductIngredient.init({
+        
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        price:{
+            type: DataTypes.DECIMAL,
+            allowNull: false,
+        },
+        quantityOnHand: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        createdAt: DataTypes.DATE,
+        updatedAt: DataTypes.DATE,
+    }, {
+        sequelize,
+        modelName: 'ProductIngredient',
+    })
+    return ProductIngredient
+
+}
+
+export default ProductIngredient
