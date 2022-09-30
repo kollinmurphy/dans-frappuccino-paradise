3fd64337906ce620289f7f2173950ae0981350a4
Trenton Peters <Trenton.Peters@usu.edu>
2022-09-30 12:12:53 -0600

    added AccountFavorite.Ts

---
+++ database/models/AccountFavorite.ts
@@ -0,0 +1,40 @@
+'use strict';
+
+import { DataTypes, InferAttributes, InferCreationAttributes, Model, Sequelize } from 'sequelize'
+
+const AccountFavorite = (sequelize: Sequelize) => {
+  class AccountFavorite extends Model<InferAttributes<AccountFavorite>, InferCreationAttributes<AccountFavorite>> {
+    declare accountId: number;
+    declare orderProductId: number;
+    declare name: string;
+    declare createdAt: Date;
+    declare updatedAt: Date;
+
+    static associate(models: any) {
+        models.AccountFavorite.belongsTo(models.Account)
+        models.AccountFavorite.belongsTo(models.OrderProduct)
+    }
+  }
+  AccountFavorite.init({
+    accountId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      orderProductId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  }, {
+    sequelize,
+    modelName: 'Account',
+  });
+  return AccountFavorite;
+};
+
+export default AccountFavorite
