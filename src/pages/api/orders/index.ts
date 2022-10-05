78f7b644aaea6b069d6c6538f9739b8861cb3813
Caden Harris <a02278419@usu.edu>
2022-10-05 12:16:47 -0600

    post Order fixed

--- src/pages/api/orders/index.ts
+++ src/pages/api/orders/index.ts
@@ -4,14 +4,12 @@ import wrapper, { Handler, AuthorizedHandler, authorizedWrapper } from "@utils/w
 type GetOrderInput = {}
 
 const postOrders: AuthorizedHandler<GetOrderInput> = async ({ user }) => {
-
     const order = await db.Order.create({
         accountId: user.id,
         status: "created",
         paid: false,
     })
     return order
-
 }
 
 export const post = authorizedWrapper(postOrders)
