3b1ffa4073269ffa14e661c8349b88a829726968
Kollin Murphy <kollin.murphy@gmail.com>
2022-10-05 11:36:07 -0600

    drink images

--- database/seeds/20221003175017-drinks.js
+++ database/seeds/20221003175017-drinks.js
@@ -32,19 +32,19 @@ module.exports = {
       { name: "Whipped cream", price: 0.10, hidden: false },
     ]))
     await queryInterface.bulkInsert('Products', addTimestamps([
-      { name: 'Coffee Frap', imageUrl: '', isDeleted: false },
-      { name: 'Pumpkin Spice Frap', imageUrl: '', isDeleted: false },
-      { name: 'Mocha Frap', imageUrl: '', isDeleted: false },
-      { name: 'Raspberry Frap', imageUrl: '', isDeleted: false },
-      { name: 'Caramel Frap', imageUrl: '', isDeleted: false },
-      { name: 'Matcha Frap', imageUrl: '', isDeleted: false },
+      { name: 'Coffee Frap', imageUrl: '/assets/coffee.jpg', isDeleted: false },
+      { name: 'Pumpkin Spice Frap', imageUrl: '/assets/pumpkinspice.jpg', isDeleted: false },
+      { name: 'Mocha Frap', imageUrl: '/assets/mocha.jpg', isDeleted: false },
+      { name: 'Raspberry Frap', imageUrl: '/assets/raspberry.jpg', isDeleted: false },
+      { name: 'Caramel Frap', imageUrl: '/assets/caramel.jpg', isDeleted: false },
+      { name: 'Matcha Frap', imageUrl: '/assets/matcha.jpg', isDeleted: false },
     ]))
     const ingredients = [4, 5, 6, 7]
     const recipes = new Map()
     recipes.set(1, [...ingredients]) // Coffee
-    recipes.set(2, [...ingredients, 8]) // Pumpkin Spice
-    recipes.set(3, [...ingredients, 12]) // Mocha
-    recipes.set(4, [...ingredients, 15]) // Raspberry
+    recipes.set(2, [...ingredients, 8, 21]) // Pumpkin Spice
+    recipes.set(3, [...ingredients, 12, 21]) // Mocha
+    recipes.set(4, [...ingredients, 15, 21]) // Raspberry
     recipes.set(5, [...ingredients, 13, 21]) // Caramel
     recipes.set(6, [...ingredients, 20, 21]) // Matcha
     for (const productId of [...recipes.keys()]) {
