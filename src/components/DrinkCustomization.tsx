e0de454f1f1eafd355c363a717fe9cc60e3d4251
Trenton Peters <Trenton.Peters@usu.edu>
2022-10-19 12:36:04 -0600

    modified:   src/components/DrinkCustomization.tsx

--- src/components/DrinkCustomization.tsx
+++ src/components/DrinkCustomization.tsx
@@ -1,11 +1,11 @@
 /* @jsxImportSource solid-js */
 
 import { Ingredient, Product } from "@data/types/product"
-import { createSignal } from "solid-js";
+import { createSignal, Show } from "solid-js";
 
 type Props = {
-    product: Product;
-    allIngredients: Array<Ingredient>;
+  product: Product;
+  allIngredients: Array<Ingredient>;
 }
 
 export default function DrinkCustomization(props: Props) {
@@ -13,9 +13,15 @@ export default function DrinkCustomization(props: Props) {
 
   let ingredientRef: HTMLSelectElement | undefined
 
-    return (
-        <div>
+  const notSelectedIngredients = () => props.allIngredients
+    .filter(i => !ingredients().some(si => si.id === i.id))
+
+  return (
+    <div>
       <div class="overflow-x-auto">
+        <Show when={ingredients().length > 5}>
+          <div>you are insane</div>
+        </Show>
         <table class="table table-zebra w-full">
           <thead>
             <tr>
@@ -24,33 +30,35 @@ export default function DrinkCustomization(props: Props) {
             </tr>
           </thead>
           <tbody>
-            
+
             {ingredients().map((i) => (
               <tr>
-                  <th>{i.name}</th>
-                  <th>
-                    <select class="select w-full max-w-xs">
-                      <option selected>1</option>
-                      <option>2</option> 
-                      <option>3</option>
-                      <option>4</option>
-                      <option>5</option>
-                    </select>
-                    </th>
-                </tr> 
+                <th>{i.name}</th>
+                <th>
+                  <select class="select w-full max-w-xs">
+                    <option selected>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>0</option>
+                  </select>
+                </th>
+              </tr>
             ))}
             <tr>
 
               <th>
                 <select ref={ingredientRef} class='text-black select select-bordered'>
-                  {props.allIngredients.map((i) => 
-                    
+                  <option selected disabled>Add Ingredient</option>
+                  {
+                    notSelectedIngredients().map((i) =>
                     <option value={i.id}>{i.name}</option>)
-                    }
+                  }
                 </select>
               </th>
               <th>
-                <button class='btn btn-primary' onClick={()=>{
+                <button class='btn btn-primary' onClick={() => {
                   const selectedIngredient = ingredientRef?.value
                   const id = parseInt(selectedIngredient)
                   const ingredient = props.allIngredients.find(i => i.id === id)
@@ -65,11 +73,11 @@ export default function DrinkCustomization(props: Props) {
           </tbody>
         </table>
       </div>
-      
+
       <button class='btn btn-secondary'>
         Place order
       </button>
-      
+
     </div>
-    )
+  )
 }
